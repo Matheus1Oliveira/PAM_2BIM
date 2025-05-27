bbd7128317e13737f12fc3fcef8c50c3fa71205b
Matheus1Oliveira <matheusoliveiranascymento@gmail.com>
2025-05-27 10:01:26 -0300

    a

--- NodeSqlite/App.tsx
+++ NodeSqlite/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { conexao, createTable } from './conf/banco';
+import { conexao, createTable, insertTable, selectTable, selectUm } from './conf/banco';
 import { useEffect } from 'react';
 import * as SQLite from "expo-sqlite";
 import * as React from 'react';
@@ -8,21 +8,48 @@ import { Button } from 'react-native-paper';
 
 export default function App() {
 
-  // HOOK
-  useEffect(() => {
-    async function main() {
-        const db = await conexao();
-    }
-    main();
-  }, []);
+  // ---- HOOK
+  useEffect(()=>{
+     async function Main() {
+        let db =  await conexao();
+        await createTable(db!);
+        // inserirUsuario(db,'Giovanna','@Giovanna');
+
+          const registro = await selectTable(db!) as Array<{id: number, nome: string, email: string}>;
+
+          for( const linhas of registro ){
+             
+              console.log(linhas.id, linhas.nome, linhas.email,)
+          }
+
+         const nome  = await selectUm(db!,1) as {id: number, nome: string, email: string};       
+
+             
+              console.log(nome.id, nome.nome,nome.email,)
+        
+
+      
+     }
+      
+     Main();
+  },[])
 
   
 
   return (
     <View style={styles.container}>
-     <Button icon="camera" mode="contained" onPress={() => createTable()}>
-    Press me
-  </Button>
+      <Button icon="camera" mode="contained" onPress={() => createTable()}>
+        cria tabela
+      </Button>
+      <Button icon="camera" mode="contained" onPress={() => insertTable("test","teste")}>
+        insert
+      </Button>
+      <Button icon="camera" mode="contained" onPress={() => selectTable()}>
+        view
+      </Button>
+      <Button icon="camera" mode="contained" onPress={() => selectUm(1)}>
+        view where
+      </Button>
     </View>
   );
 }
--- NodeSqlite/conf/banco.ts
+++ NodeSqlite/conf/banco.ts
@@ -3,24 +3,61 @@ import * as SQLite from "expo-sqlite";
 export async function conexao(){
 try{
     const db = SQLite.openDatabaseSync("PAM2");
-    console.log("Conexão com o banco de dados estabelecida:", db);
+    console.log("Conexão com o banco de dados estabelecida");
     return db;
 }catch(error){}
 }
-export async function createTable() {
+export async function createTable(db: SQLite.SQLiteDatabase) {
     try{
+      await db!.runAsync(
     `PRAGMA journal_mode = WAL;
       CREATE TABLE IF NOT EXISTS USUARIO (
-      id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
       email VARCHAR(100), 
       nome VARCHAR(100)
-      );`
+      );`);
+      console.log("tabela feita");
 
-      console.log('taberla fazida');
+      
     }catch(error){
             console.log(error);
     }
   }
+
+  export async function insertTable(db: SQLite.SQLiteDatabase, email: string, nome: string) {
+
+    try{
+          await db!.runAsync("INSERT INTO USUARIO (Nome, email) VALUES (?, ?)", [nome, email]);
+      
+    }catch(error){
+            console.log("erro");
+    }
+  }
+export async function selectTable(db: SQLite.SQLiteDatabase) {
+  
+    try{
+      const result = await db!.getAllAsync(
+    ` SELECT * FROM USUARIO`);
+    return result;
+    }catch(error){
+            console.log(error);
+    }
+  }
+   export async function selectUm(db:SQLite.SQLiteDatabase, id:number) {
+    try {
+        
+       const result = await db.getFirstAsync(' SELECT * FROM USUARIO WHERE ID_US = ?',id);
+       console.log('Filtro de Usuario por ID ' + id );
+       return result;
+
+    } catch (error) {
+         console.log('Erro ao buscar usuario ' + error);
+    }
+
+ }
+
+
+  
 
 
 
